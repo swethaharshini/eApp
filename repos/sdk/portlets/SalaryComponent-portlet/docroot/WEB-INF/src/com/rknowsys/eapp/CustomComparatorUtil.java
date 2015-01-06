b3cf28a34dcfa7605bd8b895d968a2ce4cd99b09
laxminarayanav <laxminarayana.v@rknowsys.com>
2015-01-06 12:49:28 +0530

    SalaryComponent-portlet

---
+++ repos/sdk/portlets/SalaryComponent-portlet/docroot/WEB-INF/src/com/rknowsys/eapp/CustomComparatorUtil.java
@@ -0,0 +1,30 @@
+package com.rknowsys.eapp;
+
+import com.liferay.portal.kernel.util.OrderByComparator;
+
+public class CustomComparatorUtil {
+	
+
+	
+	public static OrderByComparator getSalaryComponentOrderByComparator(
+			   String orderByCol, String orderByType) {
+			  
+			  
+			   boolean orderByAsc = false;
+			 
+			   if (orderByType.equals("asc")) {
+			   orderByAsc = true;
+			   }
+			 
+			   OrderByComparator orderByComparator = null;
+			   
+			   
+			   if (orderByCol.equalsIgnoreCase("componentName")) {
+			    
+			    orderByComparator = new ComponentComparator(orderByAsc);
+			   } 
+			  		   
+			   return orderByComparator;
+			   }
+
+}
